2d03aed438a19210dfbb411b3a27b8764540778e
arnav <arnav.bholausa@gmail.com>
2024-03-31 14:55:57 -0400

    updating content

--- src/components/Impact/Impact.jsx
+++ src/components/Impact/Impact.jsx
@@ -3,6 +3,8 @@ import styles from "./Impact.module.scss";
 import Feature from "./Feature";
 
 import Star from "../../assets/images/Impact/star.svg";
+import Teaching from "../../assets/images/Impact/teaching.svg";
+import Tree from "../../assets/images/Impact/tree.svg";
 
 const Impact = () => {
   return (
@@ -11,33 +13,33 @@ const Impact = () => {
       <div className={styles["features"]}>
         <Feature
           icon={Star}
-          number={20}
-          text='kilograms of x removed'
+          number={3}
+          text='kilograms of tetra-packs removed'
+        />
+        <Feature
+          icon={Teaching}
+          number={6}
+          text='kielograms of polythene removed'
+        />
+        <Feature
+          icon={Tree}
+          number={30}
+          text='kilograms of dry waste removed'
+        />
+        <Feature
+          icon={Tree}
+          number={18}
+          text='kilograms of ldpe, soft plastic, gunny bags removed'
         />
         <Feature
           icon={Star}
-          number={20}
-          text='kilograms of x removed'
+          number={3}
+          text='kilograms of multi-layer-plastic waste removed'
         />
         <Feature
           icon={Star}
-          number={20}
-          text='kilograms of x removed'
-        />
-        <Feature
-          icon={Star}
-          number={20}
-          text='kilograms of x removed'
-        />
-        <Feature
-          icon={Star}
-          number={20}
-          text='kilograms of x removed'
-        />
-        <Feature
-          icon={Star}
-          number={20}
-          text='kilograms of x removed'
+          number={95}
+          text='kilograms of total waste removed'
         />
       </div>
     </div>
